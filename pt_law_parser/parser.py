772808d2c7f1eaa226a11f353c754f4324360ed2
Jorge C. Leitão <jorgecarleitao@gmail.com>
2015-04-19 18:26:08 +0200

    Changed parse strategy.

--- pt_law_parser/parser.py
+++ pt_law_parser/parser.py
@@ -1,11 +1,8 @@
 import re
-import logging
-
-from fysom import Fysom
 
 from .tokenizer import tokenize
-from .expressions import Separator, Expression, DocumentReference, ArticleReference,\
-    NumberReference, LineReference
+from .expressions import Separator, Token, EndOfLine, Expression, \
+    DocumentReference, ArticleReference, NumberReference, LineReference
 
 
 DOCUMENT_NUMBER_REGEX = '^[\d\-A-Z]+/\d+(?:/[A-Z]*)?$'
@@ -14,188 +11,168 @@ NUMBER_REGEX = '^\d$|^anterior$|^seguinte$'
 LINE_REGEX = '^[\da-z]*\)$'
 
 
-class TempStorage(object):
-    def __init__(self, ref_class):
-        self._ref_class = ref_class
-        self._reference = self._ref_class('')
-        self._storage = [self._reference]
-        self._indexes = {}
+class Observer(object):
+    """
+    A Token that is able to gather information to change its own state.
 
-    def new_number(self, token, index):
-        if self._reference.number:
-            self._storage.append(self._ref_class(''))
-            self._reference = self._storage[-1]
-        self._reference.number = token
+    It contains an `index` that identifies its position in the list it belongs.
+    Uses `replace_in` to replace itself by its own `_real_class`, thus
+    terminating its observation and itself.
+    """
+    def __init__(self, index, token):
+        self._string = token.as_str()
+        self._is_done = False
+        self._index = index
 
-        assert index not in self._indexes
-        self._indexes[index] = self._reference
+    @property
+    def is_done(self):
+        return self._is_done
 
-    def clear(self):
-        self._reference = self._ref_class('')
-        self._storage = [self._reference]
-
-    def set_parent(self, temp_parent):
-        assert isinstance(temp_parent, TempStorage)
-        for ref in self._storage:
-            ref.parent = temp_parent._reference
+    def observe(self, index, token, caught):
+        raise NotImplementedError
 
     def replace_in(self, result):
-        for index in self._indexes:
-            result[index] = self._indexes[index]
+        raise NotImplementedError
 
 
-class DocTempStorage(TempStorage):
-    def __init__(self):
+class DocumentsObserver(Observer):
+
+    def __init__(self, index, token):
+        super(DocumentsObserver, self).__init__(index, token)
+
+        self._numbers = {}
+
+    def observe(self, index, token, caught):
+        if not caught and re.match(DOCUMENT_NUMBER_REGEX, token.as_str()):
+            self._numbers[index] = token
+            return True
+
+        if token in (Separator('.'), EndOfLine()):
+            self._is_done = True
+
+        return False
+
+    def replace_in(self, result):
+        type_token = Token(self._string)
+
+        for i in self._numbers:
+            result[i] = DocumentReference(self._numbers[i].as_str(), type_token)
+
+
+class ArticlesObserver(Observer):
+    klass = ArticleReference
+
+    def __init__(self, index, token):
+        super(ArticlesObserver, self).__init__(index, token)
+        self._numbers = {}
         self._parent = None
-        super(DocTempStorage, self).__init__(DocumentReference)
 
-    def set_parent(self, token):
-        self._parent = token
+    def observe(self, index, token, caught):
+        if token in (Separator('.'), EndOfLine()):
+            self._is_done = True
 
-    def new_number(self, token, index):
-        self._reference.parent = self._parent
-        super(DocTempStorage, self).new_number(token, index)
-        self._reference.parent = self._parent
+        if self._parent:
+            return False
+
+        if not caught and re.match(ARTICLE_NUMBER_REGEX, token.as_str()):
+            self._numbers[index] = token
+            return True
+        elif re.match(DOCUMENT_NUMBER_REGEX, token.as_str()):
+            self._parent = index
+
+        return False
+
+    def replace_in(self, result):
+        parent = None
+        if self._parent:
+            parent = result[self._parent]
+        for i in self._numbers:
+            result[i] = self.klass(self._numbers[i].as_str(), parent)
 
 
-def parse(string, singular_names, plural_names):
-    common_events = [('single',  'None', 'single'), ('many', 'None', 'many'),
-                     ('last', 'many', 'single'), ('clear', 'single', 'None')]
+class NumbersObserver(ArticlesObserver):
+    klass = NumberReference
 
-    document_state = Fysom(initial='None', events=common_events)
-    article_state = Fysom(initial='None', events=common_events)
-    number_state = Fysom(initial='None', events=common_events)
-    line_state = Fysom(initial='None', events=common_events)
+    def observe(self, index, token, caught):
+        if token in (Separator('.'), EndOfLine()):
+            self._is_done = True
 
-    logger = logging.getLogger(__name__ + '.organize_string')
+        if self._parent:
+            return False
 
-    temp_storage = {'doc': DocTempStorage(),
-                    'art': TempStorage(ArticleReference),
-                    'num': TempStorage(NumberReference),
-                    'lin': TempStorage(LineReference),
-                    }
+        if not caught and re.match(NUMBER_REGEX, token.as_str()):
+            self._numbers[index] = token
+            return True
+        elif re.match(ARTICLE_NUMBER_REGEX, token.as_str()):
+            self._parent = index
+        elif re.match(DOCUMENT_NUMBER_REGEX, token.as_str()):
+            self._parent = index
 
-    def start_document(token):
-        if token.as_str() in singular_names:
-            document_state.single()
-            temp_storage['doc'].set_parent(token)
-            logger.debug('changed to document.%s' % document_state.current)
-        if token.as_str() in plural_names:
-            document_state.many()
-            temp_storage['doc'].set_parent(token)
-            logger.debug('changed to document.%s' % document_state.current)
+        return False
 
-    def start_article(token):
-        if token.as_str() in ('artigo', 'Artigo'):
-            article_state.single()
-            logger.debug('changed to article.%s' % article_state.current)
-        if token.as_str() in ('artigos', 'Artigos'):
-            article_state.many()
-            logger.debug('changed to article.%s' % article_state.current)
 
-    def start_number(token):
-        # nº and n.os is also for document. Ignore if document is on.
-        if not document_state.isstate('None'):
-            return
-        if token.as_str() == 'nº':
-            number_state.single()
-            logger.debug('changed to number.%s' % number_state.current)
-        if token.as_str() == 'n.os':
-            number_state.many()
-            logger.debug('changed to number.%s' % number_state.current)
+class LineObserver(ArticlesObserver):
+    klass = LineReference
 
-    def start_line(token):
-        # nº and n.os is also for document. Ignore if document is on.
-        if token.as_str() in ('alínea', 'Alínea'):
-            line_state.single()
-            logger.debug('changed to line.%s' % line_state.current)
-        if token.as_str() in ('alíneas', 'Alíneas'):
-            line_state.many()
-            logger.debug('changed to line.%s' % line_state.current)
+    def observe(self, index, token, caught):
+        if token in (Separator('.'), EndOfLine()):
+            self._is_done = True
 
+        if self._parent:
+            return False
+
+        if not caught and re.match(LINE_REGEX, token.as_str()):
+            self._numbers[index] = token
+            return True
+        elif re.match(NUMBER_REGEX, token.as_str()):
+            self._parent = index
+        elif re.match(ARTICLE_NUMBER_REGEX, token.as_str()):
+            self._parent = index
+        elif re.match(DOCUMENT_NUMBER_REGEX, token.as_str()):
+            self._parent = index
+
+        return False
+
+
+class ObserverManager(object):
+    def __init__(self, rules):
+        self._rules = rules
+        self._observers = {}
+
+    def generate(self, index, token):
+        if token.as_str() in self._rules:
+            observer = self._rules[token.as_str()](index, token)
+            self._observers[index] = observer
+
+    @property
+    def terms(self):
+        return set(self._rules.keys())
+
+    def observe(self, index, token, caught):
+        for i, observer in self._observers.items():
+            caught = observer.observe(index, token, caught) or caught
+        return caught
+
+    def replace_in(self, result):
+        for i, observer in dict(self._observers).items():
+            if observer.is_done:
+                observer.replace_in(result)
+                del self._observers[i]
+
+
+def parse(string, managers, terms=set()):
     result = Expression()
-    for index, token in enumerate(tokenize(string, ' ,.', {'n.os'} | set(singular_names) | set(plural_names))):
+
+    for manager in managers:
+        terms |= manager.terms
+
+    for index, token in enumerate(tokenize(string, ' ,.', terms)):
         result.append(token)
-        if isinstance(token, Separator):
-            continue
-        logger.debug(token)
-        start_document(token)
-        start_article(token)
-        start_number(token)
-        start_line(token)
 
-        if re.match(DOCUMENT_NUMBER_REGEX, token.as_str()):
-            assert not document_state.isstate('None')
-            temp_storage['doc'].new_number(token, index)
-
-        if re.match(ARTICLE_NUMBER_REGEX, token.as_str()) and not article_state.isstate('None'):
-            temp_storage['art'].new_number(token, index)
-
-        if re.match(NUMBER_REGEX, token.as_str()) and not number_state.isstate('None'):
-            temp_storage['num'].new_number(token, index)
-
-        if re.match(LINE_REGEX, token.as_str()) and not line_state.isstate('None'):
-            temp_storage['lin'].new_number(token, index)
-
-        # storage and clear
-        if line_state.isstate('single') and \
-                (token == '' or article_state.isstate('single') or
-                 number_state.isstate('single')):
-            if number_state.isstate('single'):
-                temp_storage['lin'].set_parent(temp_storage['num'])
-            elif article_state.isstate('single'):
-                temp_storage['lin'].set_parent(temp_storage['art'])
-
-            temp_storage['lin'].replace_in(result)
-
-            line_state.clear()
-            temp_storage['lin'].clear()
-            logger.debug('changed to line.%s' % line_state.current)
-
-        if number_state.isstate('single') and \
-                (token == '' or article_state.isstate('single')):
-            if article_state.isstate('single'):
-                temp_storage['num'].set_parent(temp_storage['art'])
-
-            temp_storage['num'].replace_in(result)
-
-            number_state.clear()
-            temp_storage['num'].clear()
-            logger.debug('changed to number.%s' % number_state.current)
-
-        if article_state.isstate('single') and \
-                (token == '' or document_state.isstate('single')):
-            if document_state.isstate('single'):
-                temp_storage['art'].set_parent(temp_storage['doc'])
-
-            temp_storage['art'].replace_in(result)
-
-            article_state.clear()
-            temp_storage['art'].clear()
-            logger.debug('changed to article.%s' % article_state.current)
-
-        if document_state.isstate('single') and token == '':
-            temp_storage['doc'].replace_in(result)
-
-            document_state.clear()
-            temp_storage['doc'].clear()
-            logger.debug('changed to document.%s' % document_state.current)
-
-        # change from many -> last
-        if line_state.isstate('many') and token == 'e':
-            line_state.last()
-            logger.debug('changed to line.%s' % line_state.current)
-
-        if number_state.isstate('many') and token == 'e':
-            number_state.last()
-            logger.debug('changed to number.%s' % number_state.current)
-
-        if article_state.isstate('many') and token == 'e':
-            article_state.last()
-            logger.debug('changed to article.%s' % article_state.current)
-
-        if document_state.isstate('many') and token == 'e':
-            document_state.last()
-            logger.debug('changed to document.%s' % document_state.current)
+        caught = False
+        for manager in managers:
+            manager.generate(index, token)
+            caught = manager.observe(index, token, caught) or caught
+            manager.replace_in(result)
 
     return result
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,12 +2,13 @@ import unittest
 
 from pt_law_parser.expressions import Token, DocumentReference, ArticleReference, \
     NumberReference, LineReference
-from pt_law_parser.parser import parse
+from pt_law_parser.parser import parse, DocumentsObserver, \
+    ObserverManager, ArticlesObserver, NumbersObserver, LineObserver
 
 
 class GeneralTestCase(unittest.TestCase):
-    def _test(self, string, expected):
-        result = parse(string, {'Decreto-Lei'}, {'Decretos-Leis'})
+    def _test(self, string, managerss, expected):
+        result = parse(string, managerss, {'n.os'})
         self.assertEqual(string, result.as_str())
 
         for exp, index, in expected:
@@ -17,52 +18,79 @@ class GeneralTestCase(unittest.TestCase):
 class TestDocuments(GeneralTestCase):
 
     def test_single(self):
-        self._test('Decreto-Lei nº 2/2013.',
+        managers = [ObserverManager({'Decreto-Lei': DocumentsObserver})]
+
+        self._test('Decreto-Lei nº 2/2013.', managers,
                    [(DocumentReference('2/2013', Token('Decreto-Lei')), 4)])
 
-        self._test('Decreto-Lei nº 2/2013/A,',
+        self._test('Decreto-Lei nº 2/2013/A,', managers,
                    [(DocumentReference('2/2013/A', Token('Decreto-Lei')), 4)])
 
-        self._test('Decreto-Lei nº 2-A/2013,',
+        self._test('Decreto-Lei nº 2-A/2013,', managers,
                    [(DocumentReference('2-A/2013', Token('Decreto-Lei')), 4)])
 
     def test_many(self):
-        self._test('Decretos-Leis n.os 1/2006, 2/2006, e 3/2006',
+        managers = [ObserverManager({'Decretos-Leis': DocumentsObserver})]
+
+        self._test('Decretos-Leis n.os 1/2006, 2/2006, e 3/2006', managers,
                    [(DocumentReference('1/2006', Token('Decretos-Leis')), 4),
                     (DocumentReference('2/2006', Token('Decretos-Leis')), 7),
                     (DocumentReference('3/2006', Token('Decretos-Leis')), 12)])
 
-        self._test('Decretos-Leis n.os 1/2006, e 2/2006',
+        self._test('Decretos-Leis n.os 1/2006, e 2/2006', managers,
                    [(DocumentReference('1/2006', Token('Decretos-Leis')), 4),
                     (DocumentReference('2/2006', Token('Decretos-Leis')), 9)])
 
         self._test('Decretos-Leis n.os 64/2006, de 21 de março, '
                    '88/2006, de 23 de maio, e '
-                   '196/2006, de 10 de outubro',
+                   '196/2006, de 10 de outubro', managers,
                    [(DocumentReference('64/2006', Token('Decretos-Leis')), 4),
                     (DocumentReference('88/2006', Token('Decretos-Leis')), 16),
                     (DocumentReference('196/2006', Token('Decretos-Leis')), 30)])
+
+    def test_many_separated(self):
+
+        managers = [ObserverManager({'foo': DocumentsObserver,
+                                     'bar': DocumentsObserver})]
+
+        string = 'foo 1/1. bar 2/1'
+        expected = [(DocumentReference('1/1', Token('foo')), 2),
+                    (DocumentReference('2/1', Token('bar')), 7)]
+
+        self._test(string, managers, expected)
 
 
 class TestArticles(GeneralTestCase):
 
     def test_single(self):
-        self._test('no artigo 3º.', [(ArticleReference('3º'), 4)])
+        managers = [ObserverManager({'artigo': ArticlesObserver})]
 
-        self._test('no artigo 3º-A,', [(ArticleReference('3º-A'), 4)])
+        self._test('no artigo 3º.', managers, [(ArticleReference('3º'), 4)])
 
-        self._test('no artigo anterior', [(ArticleReference('anterior'), 4)])
+        self._test('no artigo 3º-A,', managers, [(ArticleReference('3º-A'), 4)])
+
+        self._test('no artigo anterior', managers,
+                   [(ArticleReference('anterior'), 4)])
 
     def test_many(self):
-        self._test('Os artigos 3º, 4º-A, 7º e 25º entram em vigor',
+
+        managers = [ObserverManager({'artigo': ArticlesObserver,
+                                     'artigos': ArticlesObserver})]
+
+        self._test('Os artigos 3º, 4º-A, 7º e 25º entram em vigor', managers,
                    [(ArticleReference('3º'), 4),
                     (ArticleReference('4º-A'), 7),
                     (ArticleReference('7º'), 10),
                     (ArticleReference('25º'), 14)])
 
     def test_with_document(self):
+
+        managers = [ObserverManager({'Decreto-Lei': DocumentsObserver}),
+                    ObserverManager({'artigos': ArticlesObserver})]
+
         doc = DocumentReference('2/2013', Token('Decreto-Lei'))
         self._test('Os artigos 3º, 4º-A, 7º e 25º do Decreto-Lei 2/2013,',
+                   managers,
                    [(ArticleReference('3º', doc), 4),
                     (ArticleReference('4º-A', doc), 7),
                     (ArticleReference('7º', doc), 10),
@@ -73,91 +101,121 @@ class TestArticles(GeneralTestCase):
 class TestNumbers(GeneralTestCase):
 
     def test_single(self):
-        self._test('no nº 2.', [(NumberReference('2'), 4)])
-        self._test('no nº 2,', [(NumberReference('2'), 4)])
-        self._test('no nº 2 ', [(NumberReference('2'), 4)])
+        managers = [ObserverManager({'nº': NumbersObserver})]
+
+        self._test('no nº 2.', managers, [(NumberReference('2'), 4)])
+        self._test('no nº 2,', managers, [(NumberReference('2'), 4)])
+        self._test('no nº 2 ', managers, [(NumberReference('2'), 4)])
 
     def test_many(self):
-        self._test('Os n.os 1 e 3 deixam', [(NumberReference('1'), 4),
-                                            (NumberReference('3'), 8)])
+        managers = [ObserverManager({'n.os': NumbersObserver})]
+
+        self._test('Os n.os 1 e 3 deixam', managers, [(NumberReference('1'), 4),
+                                                      (NumberReference('3'), 8)])
 
     def test_with_article(self):
-        art = ArticleReference('seguinte')
-        self._test('no nº 2 do artigo seguinte', [(NumberReference('2', art), 4)])
+        managers = [ObserverManager({'artigo': ArticlesObserver}),
+                    ObserverManager({'nº': NumbersObserver,
+                                     'n.os': NumbersObserver})]
 
-        self._test('nos n.os 2 e 3 do artigo seguinte',
+        art = ArticleReference('seguinte')
+        self._test('no nº 2 do artigo seguinte', managers,
+                   [(art, 10), (NumberReference('2', art), 4)])
+
+        self._test('nos n.os 2 e 3 do artigo seguinte', managers,
                    [(NumberReference('2', art), 4),
                     (NumberReference('3', art), 8)])
 
-        self._test('nos n.os 1, 2 e 3 do artigo seguinte',
+        self._test('nos n.os 1, 2 e 3 do artigo seguinte', managers,
                    [(NumberReference('1', art), 4),
                     (NumberReference('2', art), 7),
                     (NumberReference('3', art), 11)])
 
         art = ArticleReference('26º')
-        self._test('no nº 2 do artigo 26º', [(NumberReference('2', art), 4)])
+        self._test('no nº 2 do artigo 26º', managers,
+                   [(NumberReference('2', art), 4)])
 
     def test_with_document(self):
+        managers = [ObserverManager({'Decreto-Lei': DocumentsObserver}),
+                    ObserverManager({'artigo': ArticlesObserver}),
+                    ObserverManager({'nº': NumbersObserver,
+                                     'n.os': NumbersObserver})]
+
         doc = DocumentReference('2/2013', Token('Decreto-Lei'))
         art = ArticleReference('26º', doc)
         self._test('no nº 2 do artigo 26º do Decreto-Lei 2/2013,',
-                   [(NumberReference('2', art), 4)])
+                   managers, [(doc, 16), (art, 10),
+                              (NumberReference('2', art), 4)])
 
 
 class TestLines(GeneralTestCase):
 
     def test_single(self):
+        managers = [ObserverManager({'alínea': LineObserver})]
+
         line = LineReference('f)')
-        self._test('na alínea f).', [(line, 4)])
-        self._test('na alínea f),', [(line, 4)])
-        self._test('na alínea f) ', [(line, 4)])
+        self._test('na alínea f).', managers, [(line, 4)])
+        self._test('na alínea f),', managers, [(line, 4)])
+        self._test('na alínea f) ', managers, [(line, 4)])
 
     def test_many(self):
+        managers = [ObserverManager({'alíneas': LineObserver})]
+
         expectation = [(LineReference('f)'), 4), (LineReference('g)'), 8)]
 
-        self._test('nas alíneas f) e g).', expectation)
-        self._test('nas alíneas f) e g) ', expectation)
-        self._test('nas alíneas f) e g), como', expectation)
+        self._test('nas alíneas f) e g).', managers, expectation)
+        self._test('nas alíneas f) e g) ', managers, expectation)
+        self._test('nas alíneas f) e g), como', managers, expectation)
 
         expectation = [(LineReference('1)'), 4), (LineReference('5)'), 8)]
 
-        self._test('nas alíneas 1) e 5).', expectation)
-        self._test('nas alíneas 1) e 5) ', expectation)
-        self._test('nas alíneas 1) e 5), como', expectation)
+        self._test('nas alíneas 1) e 5).', managers, expectation)
+        self._test('nas alíneas 1) e 5) ', managers, expectation)
+        self._test('nas alíneas 1) e 5), como', managers, expectation)
 
         expectation = [(LineReference('aa)'), 4), (LineReference('bb)'), 8)]
 
-        self._test('nas alíneas aa) e bb).', expectation)
-        self._test('nas alíneas aa) e bb) ', expectation)
-        self._test('nas alíneas aa) e bb), como', expectation)
+        self._test('nas alíneas aa) e bb).', managers, expectation)
+        self._test('nas alíneas aa) e bb) ', managers, expectation)
+        self._test('nas alíneas aa) e bb), como', managers, expectation)
 
-    def test_with_number(self):
+    def test_with_parent(self):
+        managers = [ObserverManager({'artigo': ArticlesObserver}),
+                    ObserverManager({'nº': NumbersObserver}),
+                    ObserverManager({'alíneas': LineObserver,
+                                     'alínea': LineObserver})]
+
         parent = NumberReference('4')
-        self._test('na alínea f) do nº 4.',
+        self._test('na alínea f) do nº 4.', managers,
                    [(LineReference('f)', parent), 4), (parent, 10)])
 
         parent = NumberReference('anterior')
-        self._test('nas alíneas f) e g) do nº anterior.',
-                   [(LineReference('f)', parent), 4),
-                    (LineReference('g)', parent), 8), (parent, 14)])
+        self._test('nas alíneas f) e g) do nº anterior.', managers,
+                   [(parent, 14), (LineReference('f)', parent), 4),
+                    (LineReference('g)', parent), 8)])
 
         parent = ArticleReference('anterior')
-        self._test('nas alíneas f) e g) do artigo anterior.',
+        self._test('nas alíneas f) e g) do artigo anterior.', managers,
                    [(LineReference('f)', parent), 4),
                     (LineReference('g)', parent), 8), (parent, 14)])
 
         parent = ArticleReference('26º')
-        self._test('nas alíneas f) e g) do artigo 26º',
+        self._test('nas alíneas f) e g) do artigo 26º', managers,
                    [(LineReference('f)', parent), 4),
                     (LineReference('g)', parent), 8), (parent, 14)])
 
     def test_with_document(self):
+        managers = [ObserverManager({'Decreto-Lei': DocumentsObserver}),
+                    ObserverManager({'artigo': ArticlesObserver}),
+                    ObserverManager({'nº': NumbersObserver}),
+                    ObserverManager({'alíneas': LineObserver})]
+
         document = DocumentReference('2/2013', Token('Decreto-Lei'))
         article = ArticleReference('26º', document)
         number = NumberReference('4', article)
 
         self._test('nas alíneas f) e g) do nº 4 do artigo 26º '
-                   'do Decreto-Lei nº 2/2013',
+                   'do Decreto-Lei nº 2/2013', managers,
                    [(LineReference('f)', number), 4),
                     (LineReference('g)', number), 8), (number, 14),
                     (article, 20), (document, 28)])
