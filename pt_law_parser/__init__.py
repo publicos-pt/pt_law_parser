9815f9a6d2dbcdefe5f2fe2ede27f94d65c9ce4c
Jorge C. Leitão <jorgecarleitao@gmail.com>
2015-04-30 08:21:51 +0200

    Added objects to __init__, as part of the interface.

--- pt_law_parser/__init__.py
+++ pt_law_parser/__init__.py
@@ -1,9 +1,9 @@
-from pt_law_parser.parser import parse
-from pt_law_parser import observers
 from pt_law_parser.normalizer import normalize
+from pt_law_parser.parser import parse, common_managers, ObserverManager
+from pt_law_parser import observers
+import pt_law_parser.analyser
 from pt_law_parser.json import from_json
 from pt_law_parser.html import html_toc
-import pt_law_parser.analyser
 
 
 def analyse(text, managers, terms):
